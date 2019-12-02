3e110484fbdd38fca75e46d269c27afbe6ab7262
Alaistair Chan <alaistair@gmail.com>
2019-12-02 22:00:04 +1100

    stripping back model

---
+++ model_basic.py
@@ -0,0 +1,20 @@
+# Toy economy
+
+class Economy():
+    household = Household()
+    firm = Firm()
+
+    def cycle():
+        pass
+
+class Household():
+    self.labour = 100
+    self.money = 0
+    self.goods = 0
+    def use_labour():
+        pass
+
+class Firm():
+    self.money = 100
+    def give_wages():
+        pass
